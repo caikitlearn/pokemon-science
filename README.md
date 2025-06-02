6fe0ae1287c872a90175f13fa03745627c1f1b77
caikitlearn <caikitlearn@gmail.com>
2025-06-02 00:24:34 -0400

    adding replay parsing

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 This repo contains Pokémon-related tools, analyses, and datasets.
 
-## `get_replay_ids`
-- A command-line tool to fetch replay metadata from Pokémon Showdown for a specific format (e.g., `gen3ou`) over a defined date range. Results are saved to a CSV file named `{format_name}_replay_ids.csv`.
+## `fetch_replays.py`
+- A command-line tool to fetch replay data from Pokémon Showdown for a specific format (e.g., `gen3ou`) over a defined date range. Results are saved to a CSV file named `{format_name}_replay_ids.csv`.
 - Command-line arguments:
-	- `format_name`: 
-	- `start_date`:
-	- `end_date`:
+	- `format_name`: Pokémon Showdown format (e.g. [gen3ou](https://www.smogon.com/dex/rs/formats/ou/))
+	- `start_date`: YYYY-MM-DD start date with default 7 days before today (UTC)
+	- `end_date`: YYYY-MM-DD end date with default today (UTC)
 - Make the script executable with `chmod +x fetch_replays.py`
 - Example usage: `./fetch_replays.py gen3ou --start_date 2025-05-30 --end_date 2025-06-01`
--- fetch_replays.py
+++ fetch_replays.py
@@ -25,8 +25,123 @@ def utc_timestamp(date_str: str) -> int:
         datetime.strptime(date_str, "%Y-%m-%d").replace(tzinfo=timezone.utc).timestamp()
     )
 
+def parse_replay_log(
+    replay_id: str,
+) -> pd.DataFrame:
+    """
+    Parses a Pokémon Showdown replay log and extracts data.
 
-def fetch_replay_ids(
+    Args:
+        replay_id (str): The ID of the replay used to construct the URL of the replay JSON.
+
+    Returns:
+        pd.DataFrame: A single row containing the extracted data.
+    """
+    api_url: str = 'https://replay.pokemonshowdown.com/{}.json'
+    try:
+        api_url = f"https://replay.pokemonshowdown.com/{replay_id}.json"
+        resp = requests.get(api_url)
+        data = resp.json()
+    except Exception as e:
+        print(f"Failed to parse replay {replay_id}: {e}")
+        return {}
+
+    p1_team = {}
+    p2_team = {}
+    p1_name = None
+    p2_name = None
+    p1_elo = None
+    p2_elo = None
+    winner = None
+    p1_lead = None
+    p2_lead = None
+    p1_nick = {}
+    p2_nick = {}
+    seen_p1_lead = False
+    seen_p2_lead = False
+
+    for line in data.get('log', '').split('\n'):
+        parts = line.strip().split('|')    
+
+        if len(parts) == 1:
+            continue
+        
+        tag = parts[1]
+        # parse for player name and elo
+        if tag == 'player':
+            if parts[2] == 'p1' and not p1_name and not p1_elo:
+                p1_name = parts[3]
+                try:
+                    p1_elo = int(parts[5])
+                except ValueError:
+                    pass
+            elif parts[2] == 'p2' and not p2_name and not p2_elo:
+                p2_name = parts[3]
+                try:
+                    p2_elo = int(parts[5])
+                except ValueError:
+                    pass
+
+        # parse for team size
+        if tag == 'teamsize':
+            if parts[2] == 'p1':
+                p1_teamsize = int(parts[3])
+            elif parts[2] == 'p2':
+                p2_teamsize = int(parts[3])
+
+        # parsing the pokemon on the team
+        if tag in {'switch', 'drag'}:
+            p_data, pkmn = parts[2], parts[3]
+            pkmn = pkmn.split(',')[0]
+            p_data = p_data.split(':')
+            player = p_data[0]
+            nick = ':'.join(p_data[1:]).strip()
+            if player == 'p1a':
+                if not seen_p1_lead:
+                    p1_lead = pkmn
+                    seen_p1_lead = True
+                if pkmn not in p1_team:
+                    p1_team[pkmn] = set()
+                if nick not in p1_nick:
+                    p1_nick[nick] = pkmn
+            elif player == 'p2a':
+                if not seen_p2_lead:
+                    p2_lead = pkmn
+                    seen_p2_lead = True
+                if pkmn not in p2_team:
+                    p2_team[pkmn] = set()
+                if nick not in p2_nick:
+                    p2_nick[nick] = pkmn
+                    
+        # parsing the moves of the pokemon
+        if tag == 'move':
+            player_pkmn, move = parts[2], parts[3]
+            player, pkmn = player_pkmn.split(':')
+            pkmn = pkmn.strip()
+            if player == 'p1a':
+                p1_team[p1_nick[pkmn]].add(move)
+            elif player == 'p2a':
+                p2_team[p2_nick[pkmn]].add(move)
+
+        # parsing the winner
+        if tag == 'win':
+            winner = parts[2]
+
+    row = {
+        "p1_name": p1_name,
+        "p2_name": p2_name,
+        "p1_elo": p1_elo,
+        "p2_elo": p2_elo,
+        "winner": winner,
+        "p1_lead": p1_lead,
+        "p2_lead": p2_lead,
+        "p1_team": p1_team,
+        "p2_team": p2_team,
+    }
+    return row
+
+
+def fetch_replays(
     format_name: str,
     start_date: str,
     end_date: str,
@@ -68,7 +183,7 @@ def fetch_replay_ids(
     before = utc_timestamp(end_date)
     start_unix = utc_timestamp(start_date)
 
-    while before > start_unix:
+    while before >= start_unix:
         params = {
             "format": format_name,
             "before": before,
@@ -76,16 +191,15 @@ def fetch_replay_ids(
         success = False
         for attempt in range(1, max_retries + 1):
             try:
-                # print(f"Fetching replays (attempt {attempt})...")
                 resp = requests.get(api_url, params=params, timeout=timeout)
                 resp.raise_for_status()
                 data = resp.json()
 
                 if not data:
                     print(f"Fetched empty batch. Stopping.")
-                    return replays
+                    print(f"{tot} replay IDs fetched from {start_date} to {end_date}.")
+                    return None
 
-                replays.extend(data)
                 success = True
                 break
 
@@ -95,21 +209,29 @@ def fetch_replay_ids(
 
         if not success:
             print(f"Skipping after {max_retries} failed attempts.")
-            return replays
+            print(f"{tot} replay IDs fetched from {start_date} to {end_date}.")
+            return None
+
+        temp = min(data, key=lambda x: x['uploadtime'])['uploadtime']
+
+        if temp < start_unix:
+            data = [d for d in data if d['uploadtime'] >= start_unix]
+        tot += len(data)
+
+        for d in data:
+            parsed_data = parse_replay_log(d['id'])
+            d.update(parsed_data)
+
+        print(
+            f"Fetched {len(data)} replay IDs before "
+            f"{datetime.fromtimestamp(before, tz=timezone.utc).strftime('%F')}."
+        )
 
         fetched = pd.DataFrame(data)
-        temp = fetched["uploadtime"].min()
-
-        fetched = fetched.loc[fetched["uploadtime"] >= start_unix]
-        tot += fetched.shape[0]
-
         fetched.to_csv(
             filename, header=not os.path.isfile(filename), index=False, mode="a"
         )
-        print(
-            f"Fetched {fetched.shape[0]} replay IDs before "
-            f"{datetime.fromtimestamp(before, tz=timezone.utc).strftime('%F')}."
-        )
+
         before = temp
         time.sleep(delay)
 
@@ -152,7 +274,7 @@ def main():
     )
     args = parser.parse_args()
 
-    fetch_replay_ids(
+    fetch_replays(
         format_name=args.format_name,
         start_date=args.start_date,
         end_date=args.end_date,
